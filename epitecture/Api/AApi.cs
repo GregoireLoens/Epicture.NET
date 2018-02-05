b61a5f73252c5d83167b8e6fe32870e801153a61
loens_g <gregoire.loens@epitech.eu>
2018-02-05 18:28:07 +0100

    aapi reupload

---
+++ epitecture/Api/AApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epitecture.Api
+{
+     abstract class AApi
+    {
+        private String Token { set; get; };
+
+        public abstract void Init();
+        public abstract void LoadImage();
+        public abstract void UploadImage();
+        public abstract void SearchImage();
+    }
+}
