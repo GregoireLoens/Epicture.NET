d558d59446843ace88a99f1b5378c843447fdb0d
Hugo Soszynski <hugo.soszynski@epitech.eu>
2018-02-05 15:58:09 +0100

    Simple image GUI with ZOOOOOOOOOOOOOOOOM

--- epitecture/MainPage.xaml.cs
+++ epitecture/MainPage.xaml.cs
@@ -1,30 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System.Profile;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace epitecture
 {
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
+    public sealed partial class MainPage : Page, INotifyPropertyChanged {
+
+        private ObservableCollection<BitmapImage> _images { get; } = new ObservableCollection<BitmapImage>();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double ItemSize {
+            get => _itemSize;
+            set {
+                if (_itemSize != value) {
+                    _itemSize = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemSize)));
+                }
+            }
+        }
+        private double _itemSize;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e) {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+            AppViewBackButtonVisibility.Collapsed;
+
+            if (_images.Count == 0) {
+                await GetItemsAsync();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private async Task GetItemsAsync() {
+            StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
+            StorageFolder assets = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
+
+            IReadOnlyList<StorageFile> fileList = await assets.GetFilesAsync();
+            foreach (StorageFile file in fileList) {
+
+                if (file.ContentType == "image/png" || file.ContentType == "image/jpeg") {
+                    BitmapImage bitmapImage = new BitmapImage();
+
+                    using (IRandomAccessStream fileStream = await file.OpenReadAsync()) {
+                        bitmapImage.SetSource(fileStream);
+                    }
+                    _images.Add(bitmapImage);
+
+                }
+            }
         }
     }
 }
