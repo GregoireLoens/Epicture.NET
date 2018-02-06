75e9c4a9e088013cc0aa575b1c60f6b59b825db2
loens_g <gregoire.loens@epitech.eu>
2018-02-06 11:53:28 +0100

    flickr Api

---
+++ epitecture/Api/Flickr/FlickrApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epitecture.Api.Flikr
+{
+   public class FlickrApi:  AApi
+    {
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UploadImage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SearchImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
