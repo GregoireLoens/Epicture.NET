4a97a9a5f868f1e9ddb0e9bb6e293f43f786cee6
Jérémy Thoveron <jeremy.thoveron-@epitech.eu>
2018-02-07 17:36:42 +0100

    handling of getting images

--- epitecture/Api/Imgur/Imgur.cs
+++ epitecture/Api/Imgur/Imgur.cs
@@ -12,19 +12,32 @@ namespace epitecture.Api.Imgur
         }
 
       public void LoadImage()
-              {
-                  var method = new HttpMethod("get");
-                  var url = "https://api.imgur.com/3/gallery";
-                  var content = "";
-                  Dictionary<string, string> header = new Dictionary<string, string>
-                  {
-                      {"Authorization", "55c8986212a8f48"}
-                  };
-                  var task = Request(method, url, content, header);
-                  var response = task.Result;
-                  var result = response.Content.ReadAsStringAsync();
-                  Console.WriteLine(result.Result);
-              }
+             {
+                 var method = new HttpMethod("get");
+                 var url = "https://api.imgur.com/3/gallery/hot";
+                 var content = "";
+                 Dictionary<string, string> header = new Dictionary<string, string>
+                 {
+                     {"Authorization", "Client-ID 55c8986212a8f48"}
+                 };
+                 var task = Request(method, url, content, header);
+                 var response = task.Result;
+                 var result = response.Content.ReadAsStringAsync();
+                 var json = JsonConvert.DeserializeObject<Infos>(result.Result);
+                 foreach (var tmp in json.data)
+                 {
+                     if (tmp.images != null)
+                     {
+                         foreach (var tmp2 in tmp.images)
+                         {
+                             if (tmp2 != null)
+                             {
+                                 Console.WriteLine(tmp2.link);
+                             }
+                         }
+                     }
+                 }
+             }
 
         public override void SearchImage()
         {
